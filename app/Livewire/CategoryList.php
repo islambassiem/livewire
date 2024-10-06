8a57b86b4fbcbf0aae48936df384af4645d0c7d7
Islam Mac <islam@mac.com>
2024-10-06 23:04:11 +0300

    lesson-4

--- app/Livewire/CategoryList.php
+++ app/Livewire/CategoryList.php
@@ -3,6 +3,8 @@
 namespace App\Livewire;
 
 use App\Models\Category;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Support\Str;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,6 +12,37 @@ class CategoryList extends Component
 {
     use WithPagination;
 
+    public ?Category $category = null;
+
+    public string $name = '';
+    public string $slug = '';
+
+    public bool $showModal = false;
+
+    public function openModal()
+    {
+        $this->showModal = true;
+    }
+
+    public function updatedName(): void
+    {
+        $this->slug = Str::slug($this->name);
+    }
+
+    public function save()
+    {
+        $this->validate();
+        Category::create($this->only('name', 'slug'));
+        $this->reset('showModal');
+    }
+
+    protected function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'min:3'],
+            'slug' => ['nullable', 'string'],
+        ];
+    }
     public function render()
     {
         $categories = Category::paginate(10);
