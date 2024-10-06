ec4f8c243f95e36b3ac6470ae0fafaa823e8c856
Islam Mac <islam@mac.com>
2024-10-06 22:45:08 +0300

    lesson-3

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::factory()
+            ->count(10)
+            ->state(new Sequence(
+                fn (Sequence $sequence) => ['is_active' => collect([0, 1])->random()],
+            ))
+            ->create();
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.index')">
+                        {{ __('Categories') }}
+                    </x-nav-link>
+                    
                 </div>
             </div>
 
